0ac305ee4785d99c0b5771e4b810ebd9f0ab0d7d
Daniela2319 <frankvt04@gmail.com>
2022-09-09 20:27:27 -0300

    Estrutura de Repetição

--- Estrutura-Repetição/src/ExeBreakContinue.java
+++ Estrutura-Repetição/src/ExeBreakContinue.java
@@ -1,13 +1,25 @@
 public class ExeBreakContinue {
     public static void main(String[] args) {
-        
+
+        // Exemplo Continue
+
         String carros [] = {"Fusca 1960", "Uno 2018", "Corrolla 2006", "Chevette 1984"};
         
         for(int x = 0; x <= carros.length; x++){
             if(x == 2)
             continue;
             System.out.println(" |-------------------------------------------------------------|");
-            System.out.println("        Carro não foi escolhido para venda:  " + carros[x]);
+            System.out.println("        Carro:  " + carros[x]);
+        }
+
+        // Exemplo Break
+
+        String paes [] = {"Pão francês", "Pão Fubá", "Pão Integral", "Pão Branco"};
+
+        for(int y = 1; y <= paes.length; y++){
+            if(y == 3)
+            break;
+            System.out.println("Pãe: " + paes[y]);
         }
     }
 }
